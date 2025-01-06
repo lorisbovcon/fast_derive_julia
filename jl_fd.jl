a3ec3570af9f7a3e4e6082cad1693c0b2f6d4650
lorisbovcon <loris.bovcon0@gmail.com>
2025-01-06 17:24:00 +0100

    same as before

--- jl_fd.jl
+++ jl_fd.jl
@@ -1,18 +1,31 @@
-using Base
 import Pkg
+import Base
+
 
 struct dualno_število{T}
   realna_komponenta::T;
   dualna_komponenta::T;
 end
 
-dualno_število(x) = dualno_število(x, zero(x))
+#Base.:*(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta * y.realna_komponenta, x.realna_komponenta * y.dualna_komponenta + x.dualna_komponenta * y.realna_komponenta)
+#Base.:*(x, y::dualno_število) = dualno_število(x * y.realna_komponenta, x * y.dualna_komponenta)
+#Base.:+(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta + y.realna_komponenta, x.dualna_komponenta + y.dualna_komponenta)
+#Base.:+(x, y::dualno_število) = dualno_število(x + y.realna_komponenta, y.dualna_komponenta)
+#Base.:+(x::dualno_število, y) = dualno_število(x.realna_komponenta + y, x.dualna_komponenta)
+#Base.:-(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta - y.realna_komponenta, x.dualna_komponenta - y.dualna_komponenta)
+#Base.:-(x, y::dualno_število) = dualno_število(x - y.realna_komponenta, y.dualna_komponenta)
+#Base.:-(x::dualno_število, y) = dualno_število(x.realna_komponenta - y, x.dualna_komponenta)
+
+
+
 
 Base.:+(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta + y.realna_komponenta, x.dualna_komponenta + y.dualna_komponenta)
+
 Base.:+(x, y::dualno_število) = dualno_število(x + y.realna_komponenta, y.dualna_komponenta)
 
 Base.:-(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta - y.realna_komponenta, x.dualna_komponenta - y.dualna_komponenta)
 Base.:-(x, y::dualno_število) = dualno_število(x - y.realna_komponenta, y.dualna_komponenta)
+Base.:-(x::dualno_število, y) = dualno_število(x.realna_komponenta - y, x.dualna_komponenta)
 
 Base.:*(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta * y.realna_komponenta, (x.dualna_komponenta * y.realna_komponenta) + (y.dualna_komponenta * x.realna_komponenta))
 Base.:*(x, y::dualno_število) = dualno_število(x * y.realna_komponenta, x * y.dualna_komponenta)
@@ -20,3 +33,28 @@ Base.:*(x, y::dualno_število) = dualno_število(x * y.realna_komponenta, x * y.
 Base.:/(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta / y.realna_komponenta, (y.realna_komponenta * x.dualna_komponenta - x.realna_komponenta * y.dualna_komponenta)/(y.realna_komponenta * y.realna_komponenta))
 
 Base.:^(x::dualno_število, y::Real) = dualno_število(x.realna_komponenta ^ y, y * x.realna_komponenta ^ (y - 1) * x.dualna_komponenta)
+
+function push_forward(f, primal::Real, tangent::Real)
+  input = dualno_število(primal, tangent)
+  output = f(input)
+  primal_out = output.realna_komponenta
+  tangent_out = output.dualna_komponenta
+  return primal_out, tangent_out
+end
+
+function deriviraj(f, x::Real)
+  v = one(x)
+  _, df_dx = push_forward(f, x, v)
+  return df_dx
+end
+
+function f(x)
+  return 3 * x ^ 2 + 10 * x - 10
+end
+
+x_point = 3.0
+
+f(x_point)
+
+print(deriviraj(f, x_point))
+print("\n")
