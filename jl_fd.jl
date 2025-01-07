6da1c549001eefc1c8e65b8c3a9991116ebd9954
lorisbovcon <loris.bovcon0@gmail.com>
2025-01-07 16:05:13 +0100

    added sec, cec and added the "evaluate at" prompt

--- jl_fd.jl
+++ jl_fd.jl
@@ -1,7 +1,14 @@
 import Pkg
 import Base
 
+x_point = 3.0
+print("Enter the point to evaluate at: ")
+x_point = readline()
+x_point = tryparse(Float64, x_point)
 
+function f(x)
+  return x ^ (1/2)
+end
 
 struct dualno_število{T}
   realna_komponenta::T;
@@ -16,9 +23,6 @@ end
 #Base.:-(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta - y.realna_komponenta, x.dualna_komponenta - y.dualna_komponenta)
 #Base.:-(x, y::dualno_število) = dualno_število(x - y.realna_komponenta, y.dualna_komponenta)
 #Base.:-(x::dualno_število, y) = dualno_število(x.realna_komponenta - y, x.dualna_komponenta)
-
-
-
 
 Base.:+(x::dualno_število, y::dualno_število) = dualno_število(x.realna_komponenta + y.realna_komponenta, x.dualna_komponenta + y.dualna_komponenta)
 
@@ -37,6 +41,8 @@ Base.:^(x::dualno_število, y::Real) = dualno_število(x.realna_komponenta ^ y, 
 
 Base.:sin(x::dualno_število) = dualno_število(sin(x.realna_komponenta), cos(x.realna_komponenta))
 Base.:cos(x::dualno_število) = dualno_število(cos(x.realna_komponenta), -sin(x.realna_komponenta))
+Base.:tan(x::dualno_število) = dualno_število(tan(x.realna_komponenta), sec(x.realna_komponenta) * sec(x.realna_komponenta))
+Base.:cot(x::dualno_število) = dualno_število(cot(x.realna_komponenta), -csc(x.realna_komponenta) * csc(x.realna_komponenta))
 Base.:exp(x::dualno_število) = dualno_število(exp(x.realna_komponenta), exp(x.realna_komponenta) * x.dualna_komponenta)
 Base.:log(x::dualno_število) = dualno_število(log(x.realna_komponenta), x.realna_komponenta / x.realna_komponenta)
 
@@ -48,11 +54,6 @@ function push_forward(f, primal::Real, tangent::Real)
   return primal_out, tangent_out
 end
 
-x_point = 3.0
-
-function f(x)
-  return sin(x)
-end
 
 function deriviraj(f, x::Real)
   v = one(x)
